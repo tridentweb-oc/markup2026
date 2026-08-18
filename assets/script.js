d6fb5d9137b81341ff2fdd77117995aa0d85e1c5
takagino <hirotaka.takagi@trident.ac.jp>
2026-08-18 16:36:26 +0900

    feat: add CSS styling generator tool and update global header styles

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,8 @@ const observer = new IntersectionObserver(
     },
     {
         root: null,
-        threshold: 0.1,
-        rootMargin: '0px 0px 0px 0px'
+        threshold: 0.2,
+        rootMargin: '0px 0px -100px 0px'
     }
 );
 
